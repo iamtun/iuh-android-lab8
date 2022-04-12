a9c6d538cdc311004ef31dac6238ac4e93936116
iamtun <letuan19431791.iuh@gmail.com>
2022-04-12 12:04:08 +0700

    add ui

---
+++ app/src/main/java/fit/android/lab8_fire_base/MainActivity.java
@@ -0,0 +1,14 @@
+package fit.android.lab8_fire_base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
